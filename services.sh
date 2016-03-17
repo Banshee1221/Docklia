7cc36c0dda4efb952835d772896f66c3c4b93e44
Eugene de Beste <banshee1221@gmail.com>
2016-03-17 10:49:28 +0200

    removed unnecessary script

--- services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-ln -sf /usr/share/zoneinfo/Arfica/Johannesburg /etc/localtime
-touch /var/log/ganglia/gmetad.log
-touch /var/log/ganglia/gmond.log
-service httpd start
-gmetad -d 10 >> /var/log/ganglia/gmetad.log 2>&1 &
-gmond -d 10 >> /var/log/ganglia/gmond.log 2>&1 &
-bash
